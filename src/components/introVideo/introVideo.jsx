de7644cf6f00f468364a6ed1c2b0a36eb884de24
walkeriano <admin@MacBook-Pro-de-admin.local>
2025-10-27 20:24:34 +0100

    cambios estilos readys

--- src/components/introVideo/introVideo.jsx
+++ src/components/introVideo/introVideo.jsx
@@ -1,30 +1,36 @@
+import React, { useState } from "react";
 import styles from "./introVideo.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 export default function IntroVideo() {
-  return (
-    <section className={styles.containerVideoGeneral}>
-      <section className={styles.containerGeneral}>
-        <div className={styles.boxVideo}>
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/KpXNzbDkG3Q?si=gN3rHqfYe9UJql-R"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <button>
-          comenzar
-          <FontAwesomeIcon icon={faPaperPlane} className={styles.icon} />
-        </button>
-      </section>
+  const [videoShow, setVideoShow] = useState(true);
 
-      <span></span>
-    </section>
+  return (
+    <>
+      {videoShow && (
+        <section className={styles.containerVideoGeneral}>
+          <section className={styles.containerGeneral}>
+            <div className={styles.boxVideo}>
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/KpXNzbDkG3Q?si=gN3rHqfYe9UJql-R"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+            <button onClick={()=> setVideoShow(false)}>
+              comenzar
+              <FontAwesomeIcon icon={faPaperPlane} className={styles.icon} />
+            </button>
+          </section>
+          <span></span>
+        </section>
+      )}
+    </>
   );
 }
